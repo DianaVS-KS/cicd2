6e81ba622bce7d02a7f7f67d8065bb28549b9923
DianaVS-KS <diana.villalvazo@theksquaregroup.com>
2021-04-11 16:16:32 -0500

    fix

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,6 +3,10 @@
 #give permission for everything in the express-app directory
 sudo chmod -R 777 /home/ubuntu/cicd2
 
+# Remove previous folder
+sudo rm -rf /var/www/cicd2
+sudo mkdir /var/www/cicd2
+
 # move to git folder
 cd /home/ubuntu/cicd2
 
@@ -14,6 +18,10 @@ pm2 stop all
 sleep 1
 pm2 delete all
 sleep 1
+
+# move git folder
+sudo mv /home/ubuntu/cicd2 /var/www/
+cd /var/www/cicd2
 
 # start our node app in the background
 pm2 start index.js 
