e3d45e4ede014ccaa0a1151582ddd0b9dd3e4163
DianaVS-KS <diana.villalvazo@theksquaregroup.com>
2021-04-11 11:39:15 -0500

    borrar carpeta antes de deploy

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,6 +5,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 sudo chmod +x scripts/application_stop.sh
+sudo rm -r cicd2
 
 #create our working directory if it doesnt exist
 DIR="/home/ubuntu/cicd2"
